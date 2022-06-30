93cce5e072ec74a2863b363875f20c9407b0171f
Naola1 <naolmitiku@gmail.com>
2022-06-30 10:56:42 -0700

    C program that prints the size of various types on the computer it is compiled

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-void puts_function(void)
+/**
+ *
+ * main - Entry point.
+ * Description: C program that prints exactly "Programming is like building a,
+ * multilingual puzzle.
+ * Return: Always 0 (Success).
+ *
+ * */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
