80b4de35f3ad632c16a7f2c185b0608409467b67
Naola1 <naolmitiku@gmail.com>
2022-07-01 10:57:00 -0700

    task 5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main-a function that prints single digit number
+ *Return:success always 0
+ **/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d\n", n);
+	return (0);
+}
