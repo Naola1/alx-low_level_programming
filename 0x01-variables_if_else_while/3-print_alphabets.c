fd976cfba7b28caf925093f9af58fef19c93be5b
Naola1 <naolmitiku@gmail.com>
2022-07-01 10:44:17 -0700

    task 3 commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main-function that prints lowercase and uppercase alphabet by using putchar
+ *Return:success always 0 return
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = 97; i <= 122; i++)
+{
+		putchar (i);
+}
+for (i = 65; i <= 90; i++)
+{
+	putchar (i);
+}
+putchar (10);
+	return (0);
+}
