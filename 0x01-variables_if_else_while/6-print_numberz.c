125b7815c0c8b8c2094825254812eddcb28b83ef
Naola1 <naolmitiku@gmail.com>
2022-07-01 19:03:44 -0700

    task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main-function that prints a single digit number using putchar
+ *Return:success always 0 return
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+{
+		putchar (i);
+}
+		putchar (10);
+	return (0);
+}
