1cc472a11be380cbb4def5cd86cdaeb135cbfef8
Naola1 <naolmitiku@gmail.com>
2022-07-02 08:29:04 -0700

    task 11 commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main-a function that prints different combination of three digits
+ * Return:success always 0
+ **/
+int main(void)
+{
+	int i, j, n;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			for (n = 50; n <= 57; n++)
+			{
+				if (i < j && j < n)
+				{
+					putchar (i);
+					putchar (j);
+					putchar (n);
+					putchar (44);
+					putchar (32);
+				}
+			}
+		}
+	}
+	putchar (10);
+	return (0);
+}
