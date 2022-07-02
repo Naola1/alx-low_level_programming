07ec2bbbb72ecde1eebb7d351f3f52427dd694eb
Naola1 <naolmitiku@gmail.com>
2022-07-01 19:10:04 -0700

    task 7 commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main-function that prints reverse lowercase alphabet by using putchar
+ *Return:success always 0 return
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = 122; i <= 97; i--)
+{
+		putchar (i);
+}
+		putchar (10);
+	return (0);
+}
