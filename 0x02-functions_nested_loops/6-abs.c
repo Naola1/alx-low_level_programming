e366d2b44d126ea929b8973e90da45d362290ffb
Naola1 <naolmitiku@gmail.com>
2022-07-05 11:27:39 -0700

    task 6 commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - absolute value of int
+ * @i: int number
+ * Return: abs value
+ **/
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
