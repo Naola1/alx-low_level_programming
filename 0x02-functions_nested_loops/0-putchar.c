6de8df6ca147480b9d74661cd4e41643985e5fb7
Naola1 <naolmitiku@gmail.com>
2022-07-05 00:48:04 -0700

    0 task commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-a function that prints '_putchar'
+ * Return: success always 0
+ **/
+int main(void)
+{
+	putchar (95);
+	putchar (112);
+	putchar (117);
+	putchar (116);
+	putchar (99);
+	putchar (104);
+	putchar (114);
+	putchar (10);
+	return (0);
+}
