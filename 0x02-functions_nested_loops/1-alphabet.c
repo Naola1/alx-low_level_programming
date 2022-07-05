0c56be970b1aaef8efaa8b8915df2adb976c140c
Naola1 <naolmitiku@gmail.com>
2022-07-05 08:56:02 -0700

    adding task 1 prototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,11 +14,11 @@ int main(void)
 /*print_alphabet - a function that prints lowercase alphabet.*/
 void print_alphabet(void)
 	{
-	int i;
+	char i;
 
 	for (i = 97; i <= 122; i++)
-		putchar (i);
-	putchar (10);
+		_putchar (i);
+	_putchar (10);
 	}
 
 
