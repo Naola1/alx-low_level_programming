f80abc16ed5de6bf121daf658b2da49f9fb81b06
Naola1 <naolmitiku@gmail.com>
2022-07-07 09:20:29 -0700

    task 7 commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal-prints diagonal
+ * @n:integer parameter
+ * Return:void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+		if (i < (n - 1))
+			_putchar(10);
+	}
+	_putchar(10);
+}
