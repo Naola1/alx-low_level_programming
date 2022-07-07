3e30bee2b742a64fc6489bfea89465a6ddbd522e
Naola1 <naolmitiku@gmail.com>
2022-07-07 08:05:03 -0700

    task 2 commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul-multiplies two numbers
+ * @a:int parameter 1
+ * @b:int parameter 2
+ * Return:multiple of two numbers
+ **/
+int mul(int a, int b)
+{
+	return (a * b);
+}
