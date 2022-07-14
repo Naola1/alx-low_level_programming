7adedc495138d58dc8ff20994d8405914a5f3cc7
Naola1 <naolmitiku@gmail.com>
2022-07-14 08:08:56 -0700

    task 5 modified

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,20 @@
 #include"main.h"
 /**
  *string_toupper-lowercase to uppercase converter
- *@x :string prameter
+ *@s :string prameter
  *Return:uppercas string
  */
-char *string_toupper(char *x)
+char *string_toupper(char *s)
 {
 	int i;
 
-	while (x[i])
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (x[i] >= 97 && x[i] <= 122)
-			x[i] = x[i] - 32;
-			i++;
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
 
 	}
-	return (x);
+	return (s);
 }
