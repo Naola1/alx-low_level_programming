48c5d15ee2445e1f47a9c0a74daf85fb8e2f29b1
Naola1 <naolmitiku@gmail.com>
2022-07-14 04:01:29 -0700

    strcmp task 3 commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+/**
+ *_strcmp-compares two strings
+ *@s1:1st char* parameter
+ *@s2:2nd char* parameter
+ *Return:integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
