b77be7a5c2b81db99d61168c8fa8867619d960b5
Naola1 <naolmitiku@gmail.com>
2022-07-14 02:20:15 -0700

    task 0 commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 /**
  *_strcat-concatenates two strings.
  *@dest:first string parameter
@@ -9,17 +7,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, len1 = 0, len2 = 0;
+	int i, len = 0;
 
-	len1 = strlen(dest);
-	len2 = strlen(src);
-	for (i = 0; i <= len2; i++)
+	while (dest[len])
+		len++;
+	for (i = 0; src[i] != 0; i++)
 	{
-		if (i == len1)
-		{
-			dest[i] = ' ';
-		}
-		printf("%s", dest);
+		dest[len] = src[i];
+		len++;
 	}
+	dest[len] = '\0';
 	return (dest);
 }
