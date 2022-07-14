19704f4649a2584642a4b721b89e09e4d1ed320c
Naola1 <naolmitiku@gmail.com>
2022-07-14 12:03:25 -0700

    encodes a string task 7 commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet:a function that encodes a string
+ * @s:string parameter
+ * Return:string
+ */
+char *leet(char *s)
+{
+	int i, a = 0, l = 5;
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+	char trw[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[a])
+	{
+		i = 0;
+
+		while (i < l)
+		{
+			if (s[a] == tr[i] || s[a] - 32 == tr[i])
+					s[a] = trw[i];
+						i++;
+		}
+		a++;
+	}
+	return (s);
+}
