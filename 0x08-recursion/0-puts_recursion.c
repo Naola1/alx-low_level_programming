9edcb2a90d12393ee62b5fecf7c4df6da693e572
Naola1 <naolmitiku@gmail.com>
2022-07-20 11:12:52 -0700

    task 0 printing string using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts_recursion-printing string recursively
+ *@s:string parameter
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar (10);
+		return;
+	}
+	_putchar (*s);
+	_puts_recursion(s + 1);
+}
