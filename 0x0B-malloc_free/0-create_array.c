3d4c19579460e25bdfc83a1b6aab1f1a225915f0
Naola1 <naolmitiku@gmail.com>
2022-07-25 02:20:54 -0700

    task 0 fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *ptr = NULL;
 	unsigned int i = 0;
 
-	if (size == NULL)
+	if (size == 0)
 		return (NULL);
 
 ptr = malloc(size * (sizeof(char)));
