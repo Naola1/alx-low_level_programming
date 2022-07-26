6ee087c1131344819975a854f77838bdef10d551
Naola1 <naolmitiku@gmail.com>
2022-07-26 10:47:21 -0700

    task 1 copying a given string using malloc function done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<string.h>
+#include"main.h"
+/**
+ *_strdup-function that returns a pointer to a newly
+ * allocated space in memory,  * which contains a
+ * copy of the string given as a parameter.
+ * @str:string parameter
+ * Return:string
+ */
+char *_strdup(char *str)
+{
+int i, len;
+char *dest;
+
+len = strlen(str);
+	if (len == 0)
+		return (NULL);
+dest = malloc(len * (sizeof(char)));
+	while (i >= 0)
+	{
+		*(dest + i) = *(str + i);
+		if (*(str + i) == '\0')
+			break;
+		i++;
+	}
+	return (dest);
+}
