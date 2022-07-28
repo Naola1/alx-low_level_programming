5382733ca77cfdd5c90372cdcf67e10ff2577c8f
Naola1 <106801318+Naola1@users.noreply.github.com>
2022-07-28 21:41:31 +0300

    adding prototypes

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 
 void *malloc_checked(unsigned int b);
@@ -7,3 +10,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 
 int *array_range(int min, int max);
+
+#endif
