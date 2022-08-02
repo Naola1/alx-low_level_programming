cdd7e3885a4703ecbc7ef79bf3847a52d8379ce5
Naola1 <106801318+Naola1@users.noreply.github.com>
2022-08-02 17:11:57 +0300

    task 2 betty documentation fixed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 /**
 * main-a program that prints the name of the file
-* Return : void
+* Return : success always 0
 */
-void main(void)
+int main(void)
 {
 printf("%s\n", __FILE__);
+return (0);
 }
